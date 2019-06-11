ef7ba679377578d13af146ed0cc0b708a6f0fb22
Xavier Martinet <xavierm@fb.com>
2019-06-11 09:59:13 +0200

    debug

--- workflow.py
+++ workflow.py
@@ -94,6 +94,7 @@ def _tokenize(text_path, dictionary_to_update):
                 if token not in dictionary_to_update:
                     dictionary_to_update[token] = nb_tokens_in_dictionary
                     nb_tokens_in_dictionary += 1
+                    print(nb_tokens_in_dictionary, token)
 
     # Create tensor of size nb_tokens_in_text
     ids = torch.LongTensor(nb_tokens_in_text)
