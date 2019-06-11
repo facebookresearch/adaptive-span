b7866f8829025941e2807a24ecb7caebb30b1912
Xavier Martinet <xavierm@fb.com>
2019-06-11 18:19:30 +0200

    debug

--- experiments/test_local_small.sh
+++ experiments/test_local_small.sh
@@ -1,16 +1,6 @@
-# python main.py --data /private/home/sainbar/data/text8 \
-# --hid-sz 256 --inner-hid-sz 1024 --mem-sz 256 --batch-sz 64 --nlayers 8 \
-# --lr 0.07 --momentum 0 --dropout 0 --optim adagrad --lr-warmup 8000 \
-# --attn-lim 512 --seq --nheads 4 --grad-clip 0.03 --attn-val-mode context \
-# --attn-span-loss 0.000002 --share-pos-emb --attn-span-head --attn-span-init 0 \
-# --plot --plot-env as_test_small --nbatches 100
-#
-# python main.py --data /private/home/sainbar/data/text8 \
-# --hid-sz 256 --inner-hid-sz 1024 --mem-sz 256 --batch-sz 64 --nlayers 8 \
-# --lr 0.07 --momentum 0 --dropout 0 --optim adagrad --lr-warmup 8000 \
-# --attn-lim 512 --nheads 4 --grad-clip 0.03 \
-# --attn-span-loss 0.000002 --attn-span-head --attn-span-init 0 \
-# --plot --plot-env as_test_small2 --nbatches 100
+#!/usr/bin/env bash
+
+visdom &
 
 DATA_PATH='/private/home/xavierm/Data/text8'
 ../main.py --data $DATA_PATH \
@@ -18,4 +8,4 @@ DATA_PATH='/private/home/xavierm/Data/text8'
 --lr 0.07 --momentum 0 --dropout 0 --optim adagrad --lr-warmup 8000 \
 --attn-span-lim 512 --nheads 4 --grad-clip 0.03 \
 --attn-span-loss 0.000002 \
---plot --plot-env as_test_small3 --nbatches 100
+--plot --plot-env main --nbatches 100
