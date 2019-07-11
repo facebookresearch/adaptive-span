a8d90b8a8481ef1ae50a73b696c290aa88d34744
Sainbayar Sukhbaatar <tesatory@gmail.com>
2019-07-11 07:49:48 -0700

    typo fix

--- get_pretrained.sh
+++ get_pretrained.sh
@@ -5,7 +5,7 @@ url_root="https://dl.fbaipublicfiles.com/adaptive-span"
 mkdir -p checkpoints
 cd checkpoints
 
-wget -- "${url_root}/enwik8.pt"
-wget -- "${url_root}/text8.pt"
-wget -- "${url_root}/enwik8_large.pt"
-wget -- "${url_root}/text8_large.pt"
+wget "${url_root}/enwik8.pt"
+wget "${url_root}/text8.pt"
+wget "${url_root}/enwik8_large.pt"
+wget "${url_root}/text8_large.pt"
