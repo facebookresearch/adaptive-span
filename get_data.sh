c78525b880cadda4f775dd6ee6a4ba55522b7399
Sainbayar Sukhbaatar <tesatory@gmail.com>
2020-03-18 05:33:33 -0700

    wiki103 get data

--- get_data.sh
+++ get_data.sh
@@ -20,3 +20,16 @@ if [[ ! -d 'data/text8' ]]; then
   python prep_text8.py
   cd ../..
 fi
+
+if [[ ! -d 'data/wikitext-103' ]]; then
+  mkdir -p data
+  cd data
+  echo "Downloading wikitext-103 data ..."
+  wget --continue https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip
+  unzip -q wikitext-103-v1.zip
+  cd wikitext-103
+  mv wiki.train.tokens train.txt
+  mv wiki.valid.tokens valid.txt
+  mv wiki.test.tokens test.txt
+  cd ../..
+fi
